344d17bb9fbd6cd351f898dcdb77ecfbe4929cb4
Tsamis Konstantinos <kostisego@gmail.com>
2024-10-18 19:34:30 +0300

    changed displayed message for error

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ async function main() {
     });
     console.log("MONGO CONNECTION OPEN!");
   } catch (err) {
-    console.log("OH NO MONGO CONNECTION ERROR!");
+    console.log("MONGO CONNECTION ERROR!");
     console.log(err);
   }
 }
